9d10883f67968076eba8651b4b1fe34113ac7027
sanmi <sossesam@gmail.com>
2024-12-07 21:25:42 +0100

    all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -15,12 +15,15 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields :List[str]):
+    def __init__(self, fields: List[str]):
+        """ Returns a log message obfuscated """
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.fields = fields 
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.msg = filter_datum(self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        """ Returns a log message obfuscated """
+        record.msg = filter_datum(self.fields, self.REDACTION,
+                                  record.getMessage(), self.SEPARATOR)
         return super(RedactingFormatter, self).format(record)
 
 
