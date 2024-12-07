567470aad5fe4704613407231286d440d65678b7
sanmi <sossesam@gmail.com>
2024-12-07 09:48:54 +0100

    all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
-
 """
 Module for handling Personal Data
 """
-
-
 import re
 from typing import List
 
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
+    """ Returns a log message obfuscated """
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
