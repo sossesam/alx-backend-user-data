1c9509bb519558b8acc04269697228e074405f4b
sanmi <sossesam@gmail.com>
2024-12-07 09:45:38 +0100

    all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,5 +4,7 @@ import re
 from typing import List
 
 def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
-    return re.sub(r'(' + '|'.join(map(re.escape, fields)) + r')=[^;]*', lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
-   
+    for f in fields:
+        message = re.sub(f'{f}=.*?{separator}',
+                         f'{f}={redaction}{separator}', message)
+    return message
