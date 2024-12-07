fc3912826493cd7466e3f85a84d5f7ba1118c1c1
sanmi <sossesam@gmail.com>
2024-12-07 09:48:02 +0100

    all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python3
 
+"""
+Module for handling Personal Data
+"""
+
+
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     for f in fields:
         message = re.sub(f'{f}=.*?{separator}',
                          f'{f}={redaction}{separator}', message)
