aed1815bc580f98241661cec6f47966b14929150
sanmi <sossesam@gmail.com>
2024-12-07 09:41:52 +0100

    all

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,10 +5,3 @@ import re
 def filter_datum(fields: str, redaction: str, message: str, separator: str) -> str:
     return re.sub(r'(' + '|'.join(map(re.escape, fields)) + r')=[^;]*', lambda m: m.group(0).split('=')[0] + '=' + redaction, message)
    
-
-
-field = ["password", "date_of_birth"]
-messages = ["name=egg;email=eggmin@eggsample.com;password=eggcellent;date_of_birth=12/12/1986;", "name=bob;email=bob@dylan.com;password=bobbycool;date_of_birth=03/04/1993;"]
-
-for message in messages:
-    print(filter_datum(field, 'xxx', message, ';'))
