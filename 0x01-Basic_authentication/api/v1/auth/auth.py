2ee13bf562eaafda206cce8380d1127d40c2f3c3
sanmi <sossesam@gmail.com>
2024-12-09 20:53:38 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -9,18 +9,17 @@ from typing import TypeVar, List
 class Auth:
     """ Module of Index views
     """
-    
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Module of Index views
         """
         return False
-    
+
     def authorization_header(self, request=None) -> str:
         """ Module of Index views
         """
         return None
-    
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Module of Index views
         """
