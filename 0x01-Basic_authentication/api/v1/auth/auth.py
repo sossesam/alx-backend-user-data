e5dec382ed397d4f644efe245e4be276cd2c6a3b
sanmi <sossesam@gmail.com>
2024-12-09 22:18:20 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -32,7 +32,10 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """ Module of Index views
         """
-        return None
+        if request is None:
+            return None
+        
+        return request.headers.get("Authorization", None)
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Module of Index views
