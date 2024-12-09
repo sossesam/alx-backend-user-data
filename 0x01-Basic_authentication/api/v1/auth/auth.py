e29fa24241b44454563af3c74c1329806c22c303
sanmi <sossesam@gmail.com>
2024-12-09 20:50:26 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
+""" Module of Index views
+"""
 
 from flask import request
 from typing import TypeVar, List
 
 
 class Auth:
+    """ Module of Index views
+    """
     
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ Module of Index views
+        """
         return False
     
     def authorization_header(self, request=None) -> str:
+        """ Module of Index views
+        """
         return None
     
     def current_user(self, request=None) -> TypeVar('User'):
+        """ Module of Index views
+        """
         return None
