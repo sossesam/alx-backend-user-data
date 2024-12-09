9f1ac4a605ffa837105d05d8e43ec718420670a5
sanmi <sossesam@gmail.com>
2024-12-09 21:46:47 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -13,6 +13,20 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Module of Index views
         """
+        if path is None:
+            return True
+
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        if path[len(path)-1] != "/":
+            path += "/"
+
+        if path in excluded_paths:
+            return False
+        if path not in excluded_paths:
+            return True
+
         return False
 
     def authorization_header(self, request=None) -> str:
