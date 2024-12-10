0a6842d4a6b5f622cb6b7b4674133efe18d6c167
sanmi <sossesam@gmail.com>
2024-12-10 22:37:06 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,6 +5,7 @@
 from flask import request
 from typing import TypeVar, List
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -21,3 +22,21 @@ class BasicAuth(Auth):
 
         encoded = authorization_header.split(' ', 1)[1]
         return encoded
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str) -> str:
+        """Placehoder for documentation"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        
+        try:
+            encoded = base64_authorization_header.encode('utf-8')
+            decoded64 = base64.b64decode(encoded)
+            decoded = decoded64.decode('utf-8')
+
+            return decoded
+        except BaseException:
+            return None
+
