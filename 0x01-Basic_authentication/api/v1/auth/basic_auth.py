0e3ecf7d861e84ea6c906d8a2903f6d64a6f42ab
sanmi <sossesam@gmail.com>
2024-12-10 22:06:52 +0100

    all

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,6 +10,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """Placehoder for documentation"""
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+          """Placehoder for documentation"""
         if authorization_header is None:
             return None
         if type(authorization_header) is not str:
