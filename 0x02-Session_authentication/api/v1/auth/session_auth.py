d4f9e23e8cfc4cbeb1231bcb5f903a2d4db50914
sanmi <sossesam@gmail.com>
2024-12-22 09:29:15 +0100

    all

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,21 @@
 """ Module of Index views
 """
 from api.v1.auth.auth import Auth
-
+import uuid
 
 class SessionAuth(Auth):
     """ Module of Index views
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        if user_id is None:
+              return None
+        if type(user_id) is not str:
+              return None
+         
+        self.session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[self.session_id] = user_id
+        return self.session_id
+
+        
