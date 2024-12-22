0ea392b80a75e49e05f816d10e5b3b1d4713f755
sanmi <sossesam@gmail.com>
2024-12-22 09:32:14 +0100

    all

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -4,6 +4,7 @@
 from api.v1.auth.auth import Auth
 import uuid
 
+
 class SessionAuth(Auth):
     """ Module of Index views
     """
@@ -13,12 +14,10 @@ class SessionAuth(Auth):
         """ Module of Index views
         """
         if user_id is None:
-              return None
+            return None
         if type(user_id) is not str:
-              return None
-         
+            return None
+
         self.session_id = str(uuid.uuid4())
         self.user_id_by_session_id[self.session_id] = user_id
         return self.session_id
-
-        
