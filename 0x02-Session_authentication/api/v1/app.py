df84d852522e214fd49719aa354863bb84bc9766
sanmi <sossesam@gmail.com>
2024-12-22 12:50:26 +0100

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -56,12 +56,15 @@ def before_requesting():
         return
 
     allowed_path = ['/api/v1/status/', '/api/v1/unauthorized/',
-                    '/api/v1/forbidden/']
+                    '/api/v1/forbidden/', '/api/v1/auth_session/login/']
 
     if not auth.require_auth(request.path, allowed_path):
         return
 
     if auth.authorization_header(request) is None:
+        abort(401)
+
+    if auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
