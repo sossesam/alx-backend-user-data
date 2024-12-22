08373fc4daf1c44de4b4614672f32d9efdb99637
sanmi <sossesam@gmail.com>
2024-12-22 12:51:35 +0100

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -64,7 +64,7 @@ def before_requesting():
     if auth.authorization_header(request) is None:
         abort(401)
 
-    if auth.session_cookie(request) is None:
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
     if auth.current_user(request) is None:
