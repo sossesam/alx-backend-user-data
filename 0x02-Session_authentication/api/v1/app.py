55a6308c8f195f23e99f7761c88023c8c59e0bad
sanmi <sossesam@gmail.com>
2024-12-22 12:52:32 +0100

    all

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -61,9 +61,6 @@ def before_requesting():
     if not auth.require_auth(request.path, allowed_path):
         return
 
-    if auth.authorization_header(request) is None:
-        abort(401)
-
     if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
         abort(401)
 
