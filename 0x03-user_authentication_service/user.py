e136cd672480b8c9af54bd940220d9164368d63b
sanmi <sossesam@gmail.com>
2024-12-24 09:48:03 +0100

    all

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
+""" Placeholder for documentation"""
+
 import sqlalchemy
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
-""" Placeholder for documentation"""
+
 
 engine = create_engine('sqlite:///:memory:', echo=True)
 Base = declarative_base()
