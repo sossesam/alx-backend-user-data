06f2a6d4105a6f06b4ec64f0ece6ab9172c69496
sanmi <sossesam@gmail.com>
2025-01-17 02:30:27 +0100

    all

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,6 +18,7 @@ def index() -> str:
     """
     return jsonify({"message": "Bienvenue"})
 
+
 @app.route("/users", methods=["POST"], strict_slashes=False)
 def users() -> str:
     """POST /users
@@ -30,7 +31,8 @@ def users() -> str:
         return jsonify({"email": email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
-    
+
+
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
 def login() -> str:
     """POST /sessions
@@ -44,7 +46,8 @@ def login() -> str:
     response = jsonify({"email": email, "message": "logged in"})
     response.set_cookie("session_id", session_id)
     return response
-    
+
+
 @app.route("/sessions", methods=["DELETE"], strict_slashes=False)
 def logout() -> str:
     """DELETE /sessions
@@ -58,6 +61,7 @@ def logout() -> str:
     AUTH.destroy_session(user.id)
     return redirect("/")
 
+
 @app.route("/profile", methods=["GET"], strict_slashes=False)
 def profile() -> str:
     """GET /profile
@@ -69,6 +73,7 @@ def profile() -> str:
     if user is None:
         abort(403)
     return jsonify({"email": user.email})
+
 
 @app.route("/reset_password", methods=["POST"], strict_slashes=False)
 def get_reset_password_token() -> str:
@@ -106,12 +111,6 @@ def update_password() -> str:
     if not is_password_changed:
         abort(403)
     return jsonify({"email": email, "message": "Password updated"})
-
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="5000")
-
-
 
 
 if __name__ == "__main__":
