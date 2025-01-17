8cf1d4e2ec8b899497ec4fbb1e484880498a662d
sanmi <sossesam@gmail.com>
2025-01-17 03:59:21 +0100

    all

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -26,13 +26,16 @@ def users():
             - email
             - password
     """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    try:
-        AUTH.register_user(email, password)
-        return jsonify({"email": email, "message": "user created"})
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
+    if request.method == "POST":
+        email = request.form.get("email")
+        password = request.form.get("password")
+        try:
+            AUTH.register_user(email, password)
+            return jsonify({"email": email, "message": "user created"})
+        except ValueError:
+            return jsonify({"message": "email already registered"})
+    else:
+        abort(400)
 
 
 @app.route("/sessions", methods=["POST"], strict_slashes=False)
