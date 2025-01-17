aa2285d4e02d3051b8f2139b2b5b11d6cbd8bc2f
sanmi <sossesam@gmail.com>
2025-01-17 01:23:32 +0100

    all

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,6 @@ class DB:
         """Initialize a new DB instance
         """
         self._engine = create_engine("sqlite:///a.db", echo=False)
-        Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
@@ -45,24 +44,27 @@ class DB:
 
     def find_user_by(self, **kwargs) -> User:
             """Finds a user based on a set of filters.
+
             """
-            fields, values = [], []
-            for key, value in kwargs.items():
-                if hasattr(User, key):
-                    fields.append(getattr(User, key))
-                    values.append(value)
-                else:
-                    raise InvalidRequestError()
-            result = self._session.query(User).filter(
-                tuple_(*fields).in_([tuple(values)])
-            ).first()
-            if result is None:
-                raise NoResultFound()
-            return result
-    
+            
+            user = self._session.query(User).filter_by(**kwargs).first()
+           
+            if user:
+                return user
+            else:
+                raise NoResultFound
+
+            
+                
+
+
+
+            
+            
+            
+    """
     def update_user(self, user_id: int, **kwargs) -> None:
-        """Updates a user based on a given id.
-        """
+        
         user = self.find_user_by(id=user_id)
         if user is None:
             return
@@ -77,3 +79,4 @@ class DB:
             synchronize_session=False,
         )
         self._session.commit()
+        """
