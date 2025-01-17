683003900fb1e94babf3ba25484453e6b31a3f89
sanmi <sossesam@gmail.com>
2025-01-17 03:46:01 +0100

    all

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -63,14 +63,18 @@ class Auth:
         user = None
         try:
             user = self._db.find_user_by(email=email)
+
+            if user is None:
+                return None
+        
+            session_id = _generate_uuid()
+            self._db.update_user(user.id, session_id=session_id)
+            return session_id
+
         except NoResultFound:
             return None
-        if user is None:
-            return None
-        session_id = _generate_uuid()
-        self._db.update_user(user.id, session_id=session_id)
-        return session_id
 
+        
     def get_user_from_session_id(self, session_id: str) -> Union[User, None]:
         """Retrieves a user based on a given session ID.
         """
